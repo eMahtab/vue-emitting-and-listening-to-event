52557cad53f2319f250256998815e8873a753166
Mahtab Alam <alammahtab08@gmail.com>
2018-04-20 23:46:57 +0530

    Example showing how to pass updated values from child component to parent component by emitting and listening to event

---
+++ src/components/Child1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="child1" class="component">
+    
+    <h2>Child 1 Component</h2>
+    <span>{{message}}</span><br/><br/>
+    <button @click="change">Change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message'],
+  methods: {
+    change(){  // Equivalent to change : function (){}
+      this.message = "Changed";
+      this.$emit('messageChanged',this.message);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#child1 {
+  background-color: lightseagreen;
+  
+}
+</style>
